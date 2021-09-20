f47fe2a635b4edc14e98dc7eb5333463378f3287
satishkumpatla <satishkumpatla1999@gmail.com>
2021-09-20 10:56:01 +0530

    Merge branch 'reportfeature'

--- sampleaap/src/main/java/org/cap/demo/BootClass.java
+++ sampleaap/src/main/java/org/cap/demo/BootClass.java
@@ -5,7 +5,11 @@ public class BootClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("From STS to Git");
-
+		show();
+		
+	}
+	public static void show() {
+		System.out.println("Method displayed");
 	}
 
 }
